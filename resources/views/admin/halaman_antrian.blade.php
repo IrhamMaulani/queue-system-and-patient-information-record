fc869327ea9e5b1b94d7ab2eebd09e709f2af941
IrhamMaulani <irhammaulani@gmail.com>
2018-09-22 12:30:31 +0800

    penambahan halaman kontrol antrian

---
+++ resources/views/admin/halaman_antrian.blade.php
@@ -0,0 +1,24 @@
+@extends('layout/template_awal')
+
+@section('judul')
+Sistem Antrian Puskesmas Alalak Tengah
+    
+@endsection
+
+@section('isi_web')
+
+<div class="row p-2">
+    <div class="col-lg-4 bg-success">
+        
+    </div>
+    <div class="col-lg-2 bg-warning">
+        <p>2018 - 02 - 31 10:10 Wita</p>
+        </div>
+        <div class="col-lg-6 bg-info ">
+                <p style="margin-left : 50% ">Nomor Antrian Saat Ini:</p>
+                <p>ada</p>
+                </div>
+</div>
+
+@endsection
+
